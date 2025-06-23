2cd9d423ff163954a7688ed88d106e837c52aae1
SimonB <Simon_Baeuerle@web.de>
2025-06-23 17:07:05 +0200

    generate blank pendulum_integrator file for lecture. Removed full screen featuer

--- pendulum_integrator_ref.py
+++ pendulum_integrator_ref.py
@@ -7,19 +7,26 @@ L = 0.5
 m = 0.2
 d =  0.2
 
-# Zeit
+# Anfangsbedingungen
 t_end = 5
-h = 0.1
+h = 0.01
 
+
+# Anfangsbedingungen
+theta_0 = np.deg2rad(45)
+dtheta_0 = 0 
 
 # Speicherplatz schonmal belegen
 t_values = np.arange(0, t_end, h)
-number_of_steps = len(t_values)-1
 u = np.zeros((2, len(t_values)))
 
-# Anfangsbedingungen
 
-# Euler explizit
+u[0,0] = theta_0
+u[1,0] = dtheta_0
+
+for n in range(0, len(t_values)-1):
+    u[0, n+1] = u[0,n] + h*u[1,n]
+    u[1, n+1] = u[1,n] + h*(-d/m*u[1,n]-g/L*np.sin(u[0,n]))                            
 
 
 show_reference = True
